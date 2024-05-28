f37b4971922d60a3637f90eb6257bf01f9159875
mithun <mithunweerasinghe999@gmail.com>
2024-05-29 01:23:43 +0530

    changed according to capabling to get info from UI

--- src/main/java/com/mithun/student_management/service/StudentService.java
+++ src/main/java/com/mithun/student_management/service/StudentService.java
@@ -2,6 +2,9 @@ package com.mithun.student_management.service;
 
 import com.mithun.student_management.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudent();
 }
